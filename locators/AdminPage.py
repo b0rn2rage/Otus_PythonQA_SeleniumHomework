1e8b3e2fef881820bd1ad04010498e4850c0ccdc
Anton <b0rn2rage@bk,ru>
2021-02-15 22:07:27 +0300

    Добавление двух тестов по разделу с админкой

--- locators/AdminPage.py
+++ locators/AdminPage.py
@@ -7,3 +7,11 @@ class AdminPage:
     LOGIN_BUTTON = (By.CSS_SELECTOR, "button.btn")
     FORGOTTEN_PASSWORD = (By.XPATH, "//a[text()='Forgotten Password']")
     GO_TO_HOMEPAGE = (By.CSS_SELECTOR, "[title='OpenCart']")
+    CONTAINER = (By.CSS_SELECTOR, "#container")
+    VIEW_LATEST_ORDERS = (By.CSS_SELECTOR, ".fa.fa-eye")
+    PRINT_SHIPPING_LIST = (By.CSS_SELECTOR, "[data-original-title='Print Shipping List']>.fa.fa-truck")
+    SALES = (By.CSS_SELECTOR, ".fa.fa-shopping-cart.fw")
+    SECTIONS_IN_SALES = (By.CSS_SELECTOR, "#collapse26>li>a")
+    ADD_NEW_ORDER = (By.CSS_SELECTOR, ".fa.fa-plus")
+    CUSTOMER_FIELD = (By.CSS_SELECTOR, "#input-customer")
+
--- tests/test_adminpage.py
+++ tests/test_adminpage.py
@@ -1,6 +1,7 @@
 from locators.AdminPage import AdminPage
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
 def test_adminpage(browser):
@@ -11,4 +12,36 @@ def test_adminpage(browser):
     wait.until(EC.visibility_of_element_located(AdminPage.LOGIN_BUTTON))
     wait.until(EC.visibility_of_element_located(AdminPage.FORGOTTEN_PASSWORD))
     wait.until(EC.visibility_of_element_located(AdminPage.GO_TO_HOMEPAGE))
+
+
+def test_print_shipping_list(browser):
+    browser.get("https://demo.opencart.com/admin/")
+    wait = WebDriverWait(browser, 5)
+    login_button = wait.until(EC.visibility_of_element_located(AdminPage.LOGIN_BUTTON))
+    login_button.click()
+    wait.until(EC.visibility_of_element_located(AdminPage.CONTAINER))  # Жду загрузку страницы
+    latest_orders = browser.find_elements(*AdminPage.VIEW_LATEST_ORDERS)
+    latest_orders[0].click()
+    wait.until(EC.visibility_of_element_located(AdminPage.CONTAINER))
+    print_shipping_button = wait.until(EC.visibility_of_element_located(AdminPage.PRINT_SHIPPING_LIST))
+    print_shipping_button.click()
+    try:
+        wait.until(EC.visibility_of_element_located(AdminPage.CONTAINER))
+    except TimeoutException:
+        raise AssertionError()
+
+
+def test_open_add_new_order_form(browser):
+    browser.get("https://demo.opencart.com/admin/")
+    wait = WebDriverWait(browser, 5)
+    login_button = wait.until(EC.visibility_of_element_located(AdminPage.LOGIN_BUTTON))
+    login_button.click()
+    wait.until(EC.visibility_of_element_located(AdminPage.CONTAINER))
+    browser.find_element(*AdminPage.SALES).click()
+    wait.until(EC.visibility_of_element_located(AdminPage.SECTIONS_IN_SALES))
+    sections_in_sales = browser.find_elements(*AdminPage.SECTIONS_IN_SALES)
+    sections_in_sales[0].click()
+    add_new_order = wait.until(EC.visibility_of_element_located(AdminPage.ADD_NEW_ORDER))
+    add_new_order.click()
+    wait.until(EC.visibility_of_element_located(AdminPage.CUSTOMER_FIELD))
 
